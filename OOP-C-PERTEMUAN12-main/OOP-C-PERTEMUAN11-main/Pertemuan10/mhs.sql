6bb500b59626184415f47bf4596d25686ebee3cf
yatisptrn <165276895+yatisptrn@users.noreply.github.com>
2025-06-14 14:51:19 +0800

    Add files via upload

---
+++ OOP-C-PERTEMUAN12-main/OOP-C-PERTEMUAN11-main/Pertemuan10/mhs.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2025 at 08:42 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhs`
+--
+
+CREATE TABLE `mhs` (
+  `NAMA` varchar(30) NOT NULL,
+  `NIM` varchar(10) NOT NULL,
+  `JURUSAN` enum('KAB','MTI','DKV','TIPAR','BD','RSK') NOT NULL,
+  `ALAMAT` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mhs`
+--
+
+INSERT INTO `mhs` (`NAMA`, `NIM`, `JURUSAN`, `ALAMAT`) VALUES
+('Yunita Cahya', '2301010021', 'KAB', 'Jl. Lantang Hidung'),
+('YATSU', '2301010023', 'KAB', 'OLE'),
+('Ni Luh Yati Sugiri Putriningsi', '2301010024', 'KAB', 'Jl. Tukad Pancoran'),
+('Yati Sugiri', '2301010338', 'KAB', 'Jl Tukad Bilok');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mhs`
+--
+ALTER TABLE `mhs`
+  ADD PRIMARY KEY (`NIM`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ OOP-C-PERTEMUAN12-main/OOP-C-PERTEMUAN11-main/Pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,17 @@
+package pertemuan10;
+
+/**
+ *
+ * @author NI LUH YATI SUGIRI PUTRININGSIH
+ * TGL: 24 MEI 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
